63530edbc830e6d1a80eba6fa5978cff971ca6d7
IT24100499 <IT24100499@my.sliit.lk>
2024-08-29 23:35:10 +0530

    Add files via upload

---
+++ Q3/IT24100499Lab6Q3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class IT24100499Lab6Q3{
+	public static void main(String[] args){
+		//getting the inputs from the user 
+			Scanner scanner = new Scanner(System.in);
+
+		double sum = 0;
+		int counter = 0;
+		int number;
+	
+		//guid user to get the right inputs
+			System.out.println("Enter positive integers (terminate input with -99)");
+
+		//geting the inputs from the user by using the while loop
+		while(true){ 
+			System.out.println("Enter the number:");
+			number = scanner.nextInt();
+			
+		//check if the number is -99		
+		if(number== -99){
+			break;
+		    }	
+		//check if the number is positive or negative
+		if (number < 0 ){
+		    	System.out.println("The entered number is invalid.Enter the positive number");
+		    }		
+		sum += Math.pow(number, 2);
+		counter++;
+		}
+		
+
+		//calculate and check the validation of the numnebers
+		if(counter > 0){
+			double RMS = Math.sqrt(sum / counter);
+			System.out.println("The root mean square is (RMS)" + RMS);
+		}else {
+			System.out.println("There is not valid numbers entered");		
+		}
+			
+
+
+
+
+
+	
+	
+
+
+	
+	}
+}
